bce826f288ab541c20850be1b9705e180dc51a3e
Parker Manning <parkerlmanning@hotmail.com>
2022-02-09 10:55:29 -0600

    working redux / all slices communicating properly to display quiz/cards

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import topicsReducer from './../features/topics/TopicSlice';
 import QuizSlice from "../features/quizzes/QuizSlice";
+import CardSlice from "../features/cards/CardSlice";
 
 
 export default configureStore({
   reducer: {
     topics: topicsReducer,
-    quizzes: QuizSlice
+    quizzes: QuizSlice,
+    cards: CardSlice,
   },
 });
--- src/features/quizzes/Quiz.js
+++ src/features/quizzes/Quiz.js
@@ -1,23 +1,46 @@
 import { Link, useParams } from "react-router-dom";
 import Card from "../cards/Card";
 import ROUTES from "../../app/routes";
+import { useSelector } from 'react-redux';
+import { selectQuiz } from "./QuizSlice";
+import { selectCards } from './../cards/CardSlice';
+
 
 export default function Topic() {
-  const quizzes = {}; // replace this with a call to your selector to get all the quizzes in state
+  const { quizzes } = useSelector(selectQuiz);
   let { quizId } = useParams();
-  const quiz = quizzes[quizId];
 
+
+  const matchId = (quiz) => {
+    if (quiz.id === quizId) {
+      return quiz
+    }
+  } 
+  const quiz = quizzes.quizzes.filter(matchId);
+  
   return (
-    <section>
-      <h1>{quiz.name}</h1>
-      <ul className="cards-list">
-        {quiz.cardIds.map((id) => (
-          <Card key={id} id={id} />
-        ))}
-      </ul>
-      <Link to={ROUTES.newQuizRoute()} className="button center">
-        Create a New Quiz
-      </Link>
-    </section>
+    <>
+      {
+        quiz ? (
+          <section>
+            {quiz.map((quiz) => (
+              <div key={quiz.id}>
+                <h1>{quiz.name}</h1>
+                <ul className="cards-list">
+                  {quiz.cardIds.map((id) => (
+                    <Card key={id} id={id} />
+                  ))}
+                </ul>
+                <Link to={ROUTES.newQuizRoute()} className="button center">
+                  Create a New Quiz
+                </Link>
+              </div>
+            ))}
+          </section>
+
+        ) : (
+          <div> loading .... </div>
+        )}
+    </>
   );
 }
